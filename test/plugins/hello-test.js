443532e972e2f7e65a94fef039881e29f7dc85c0
Sara Bee <sara.bainbridge@gmail.com>
2016-02-13 10:27:10 -0500

    Add some test infrastructure

---
+++ test/plugins/hello-test.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+const muxer = require('../../utils/muxer');
+
+describe('hello plugin', () => {
+
+  it('should do anything', () => {
+    console.log('foo');
+  });
+});
