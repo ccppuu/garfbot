8633110c53d7c2d0ee8f8823f66285bd37b45580
Sara Bee <sara.bainbridge@gmail.com>
2016-02-13 18:26:50 -0500

    :bug:s

--- utils/db.js
+++ utils/db.js
@@ -1,6 +1,21 @@
 const config = require('config');
 const makeURL = require('mongo-make-url');
 const mongoose = require('mongoose');
+const logger = require('./logger');
 
 const mongoURL = makeURL(config.mongo);
-return mongoose.connect(mongoURL);
+logger.info(`Connecting to mongo at ${mongoURL}`);
+
+const db = mongoose.connect(mongoURL);
+const connection = mongoose.connection;
+
+connection.on('error', err => {
+  logger.error('A database error occurred');
+  logger.error(err);
+});
+
+connection.once('open', () => {
+  logger.info(`Connected to mongo at ${mongoURL}`);
+});
+
+module.exports = connection;
