40d66340b3850218ac6158a074ba1e61b86e6529
Sara Bee <sara.bainbridge@gmail.com>
2016-02-13 19:12:11 -0500

    More tidying up :bath:

--- models/friend-schema.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const FriendSchema = mongoose.Schema({
-  twitter_handle: String,
-  slack_id: String
-});
-
-module.exports = mongoose.model('Friend', FriendSchema);;
