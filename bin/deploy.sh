9ff2b85a7081ccd7be9774d09133c50c0f20a30e
Sara Bee <sara.bainbridge@gmail.com>
2016-02-15 17:11:28 -0500

    Add rsync, etc. to deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -26,7 +26,22 @@ cat << "GARF_SENTINEL"
                         `"""`
 GARF_SENTINEL
 
-
-
-
 echo "DEPLOYING GARFWEB TO $GARFWEB_HOST AS $GARFWEB_USER"
+
+bin_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
+garfbot_path=$bin_path/..
+
+echo "Copying garf files..."
+rsync -av --delete --progress --stats \
+      --exclude 'node_modules' --exclude 'config/local.json' \
+      $garfbot_path circleci@garfweb:~/garfbot
+
+echo "Garf copy complete."
+
+echo "Installing garf packages..."
+ssh $GARFWEB_USER@$GARFWEB_HOST -t 'bash -l -c "cd garfbot && npm install"'
+echo "Garf install complete."
+
+echo "Copying garf secrets..."
+ssh $GARFWEB_USER@$GARFWEB_HOST -t 'bash -l -c "cp ~/secrets.json garfbot/config/local.json"'
+echo "Garf secrets copied."
