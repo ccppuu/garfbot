adf269f7a9c579e6b7a468fec36a6d28bb9da01d
Sara Bee <sara.bainbridge@gmail.com>
2016-12-01 19:10:47 -0500

    Add mongo and slackbot instructions

--- config/local-template.js
+++ config/local-template.js
@@ -1,3 +1,19 @@
 module.exports = {
+  mongo: {
+    db: 'garfbot-dev'
+    user: 'garfbot',
+    // TODO: Fill this in with he database password for your 'garfbot' user
+    // in the garfbot database.
+    password: null,
+    // TODO: add the host for your mlab database here. For example:
+    //    hosts: ["ds123456.mlab.com:98765"]
+    hosts: [],
+  },
 
+  slack: {
+    // TODO: Fill this in with your Slack API Token. You can find it at
+    // https://goodsonicfanart.slack.com/apps/A0F7YS25R-bots
+    // under your bot's config.
+    api_token: null,
+  },
 };
