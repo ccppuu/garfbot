5421b55f06506ef73277897eef24ce83b9093b6a
Daniel <daniel@binaryparadox.net>
2016-12-05 18:02:57 -0500

    Updates deploy script.
    Two primary changes:
    
    1. The config files have a `.js` extension these days, not `.json`
    2. I added support for a `GARFWEB_PORT` env variable because the current
       "prod" server runs its SSHD on a non-default port (tinfoil hats r us)

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -26,22 +26,23 @@ cat << "GARF_SENTINEL"
                         `"""`
 GARF_SENTINEL
 
-echo "DEPLOYING GARFWEB TO $GARFWEB_HOST AS $GARFWEB_USER"
+echo "DEPLOYING GARFWEB TO $GARFWEB_HOST:$GARFWEB_PORT AS $GARFWEB_USER"
 
 bin_path=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
 garfbot_path=$bin_path/..
 
 echo "Copying garf files..."
 rsync -av --delete --progress --stats \
-      --exclude 'node_modules' --exclude 'config/local.json' \
+      --exclude 'node_modules' --exclude 'config/local.js' \
+      -e "ssh -p $GARFWEB_PORT" \
       $garfbot_path $GARFWEB_USER@$GARFWEB_HOST:~/garfbot
 
 echo "Garf copy complete."
 
 echo "Installing garf packages..."
-ssh $GARFWEB_USER@$GARFWEB_HOST -t 'bash -l -c "cd garfbot && npm install"'
+ssh -p $GARFWEB_PORT $GARFWEB_USER@$GARFWEB_HOST -t 'bash -l -c "cd garfbot && npm install"'
 echo "Garf install complete."
 
 echo "Copying garf secrets..."
-ssh $GARFWEB_USER@$GARFWEB_HOST -t 'bash -l -c "cp ~/secrets.json garfbot/config/local.json"'
+ssh -p $GARFWEB_PORT $GARFWEB_USER@$GARFWEB_HOST -t 'bash -l -c "cp ~/secrets.js garfbot/config/local.js"'
 echo "Garf secrets copied."
