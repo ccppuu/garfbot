d28a07a6e1267590315dbb3f4eb349a1e23495e9
Daniel McCarney <daniel@binaryparadox.net>
2016-12-08 18:53:18 -0500

    mktheme plugin v0.1 (#41)
    * mktheme plugin v0.1
    
    * lint fixes
    
    * Doeg improvements :heart:
    
    * Kicking CI commit
    
    * Fixes no hue invocation

---
+++ plugins/mktheme.js
@@ -0,0 +1,36 @@
+const logger = require('../utils/logger');
+const slackAPI = require('../lib/slack-api');
+const randomColour = require('randomcolor');
+const values = require('lodash/values');
+const config = require('config');
+
+// There are 8 colours to a Slack theme
+const NUM_THEME_COLOURS = 8;
+
+const regex = /mktheme( [\w]+)?/;
+
+module.exports = {
+  regex,
+
+  description: 'Garf generates a random Slack theme for you',
+
+  requirePrefix: true,
+
+  fn(message) {
+    var opts = {
+      count: NUM_THEME_COLOURS
+    };
+
+    const matches = regex.exec(message.text);
+    if (matches.length >= 2 && matches[1] !== undefined) {
+      const hue = matches[1].trim();
+      opts.hue = hue;
+    }
+
+    const colours = randomColour(opts);
+    logger.info(colours);
+
+    const theme = values(colours).join(',');
+    return Promise.resolve(theme);
+  }
+};
